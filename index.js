e1fc924bcc823b53579db44eb291f43700556785
EzeBernardine <demayous2@gmail.com>
2018-07-14 17:08:23 +0100

    the worst state

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-var dice = {
-    sides: 6,
-    roll: function () {
-        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-        return randomNumber;
-    }
-}
-
-function printNumber(number) {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = number;
-
-    let sum = []
-    let resume = document.querySelectorAll(".resume")
-    sum.push(number)
-    for (let i = 0; i < resume.length; i++) {
-        resume[i].addEventListener("dblclick", resumeFunc);
-        function resumeFunc() {
-            if (number === 6) {
-                resume[i].style.backgroundColor = 'pink'
-            }
-            console.log(sum)
-        }
-    }
-}
-
+let placeholder = document.getElementById('placeholder');
 var button = document.getElementById('button');
+let resume = document.querySelectorAll(".resume")
+
+let sum = [];
 button.onclick = function () {
-    var result = dice.roll();
-    printNumber(result);
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    placeholder.innerHTML = randomNumber;
+    sum.push(randomNumber)
+    let lastNumber = (sum.length) - 1;
+    // console.log(sum)
+    // lastNumber = (sum.length) - 1;
+    console.log(`the last number is ` + sum[lastNumber])
+    let me = []
+    resume.forEach((each) => {  
+        me.push(each)
+    })
+            if (randomNumber === 6) {
+                console.log(me)
+                me.filter
+                me.addEventListener("dblclick", resumeFunc);
+                // console.log(me)
+                function resumeFunc() {
+                    console.log(randomNumber)
+                   me.style.backgroundColor = 'pink'
+                    console.log('hello')
+                }
+            }
+
 };
+
+// console.log(sum )
+
 
 
 
@@ -39,16 +41,11 @@ button.onclick = function () {
 
 let general = document.querySelectorAll(".general")
 let arr = [];
-let lent;
 for (let i = 0; i < general.length; i++) {
-    general[i].addEventListener('click', func)
-
-    function func() {
+    general[i].addEventListener('click', function () {
         general[i].className = "green";
         arr.push(general[i]);
-        lent = (arr.length) - 2
-        arr[lent].className = "general"
-        console.log(arr[lent])
-        console.log(general[i])
-    }
+        let lent = (arr.length) - 2 
+        {arr[lent] != general[i] && (arr[lent].className = "general") } 
+    })
 }
