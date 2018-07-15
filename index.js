7eb734880b07d16d3de3c56ba357b4b5733ac1a2
EzeBernardine <demayous2@gmail.com>
2018-07-15 19:17:31 +0100

    removal of yellow tracker and yeloow-unlock-paragrapgph text

--- index.js
+++ index.js
@@ -21,16 +21,14 @@ let blueClassName = 1
     greenClassName++
     document.getElementsByClassName("green-tracker")[0].innerHTML = greenClassName ;
    }
-    
-// let unlock = document.querySelectorAll(".unlock")
+
 let arrayFromAllRolledDice = [];
 let allRedDiceArr = []
+let allYellowDiceArr = []
 button.onclick = function () {
     let randomNumber = Math.floor(Math.random() * 6) + 1;
     placeholder.innerHTML = randomNumber;
     arrayFromAllRolledDice.push(randomNumber)
-    
-        
         
         if (randomNumber === 6 ) {
             let allRedDiceDiv = document.querySelectorAll(".red")
@@ -41,20 +39,29 @@ button.onclick = function () {
                     e.target.parentElement.removeChild(red)
                     handleRedTracker()
                     if(allRedDiceArr.length === 1){
-                      let unlockRed =  document.getElementsByClassName("unlockRed")[0];
-                      unlockRed.parentElement.removeChild(unlockRed);
+                      let unlockRedPara =  document.getElementsByClassName("unlockRed")[0];
+                      unlockRedPara.parentElement.removeChild(unlockRedPara);
                       let redTracker =  document.querySelector(".red-tracker");
                       redTracker.parentElement.removeChild(redTracker)
                     }
                 }
                 allRedDiceArr = []
             } )
+            let allYellowDiceDiv = document.querySelectorAll(".yellow")
+            allYellowDiceDiv.forEach((each) => allYellowDiceArr.push(each))
             let  yellow = document.getElementsByClassName(`yellow${yellowClassName}`)[0]
             yellow.addEventListener("click",function(e){
                 if(arrayFromAllRolledDice[arrayFromAllRolledDice.length-1] === 6){
                      e.target.parentElement.removeChild(yellow)
                      handleYellowTracker()
+                     if(allYellowDiceArr.length === 1){
+                        let unlockYellowPara =  document.getElementsByClassName("unlockYellow")[0];
+                        unlockYellowPara.parentElement.removeChild(unlockYellowPara);
+                        let yellowTracker =  document.querySelector(".yellow-tracker");
+                        yellowTracker.parentElement.removeChild(yellowTracker)
+                      }
                 }
+                allYellowDiceArr = []
             } )
             let green = document.getElementsByClassName(`green${greenClassName}`)[0]
             green.addEventListener("click",function(e){
@@ -70,23 +77,11 @@ button.onclick = function () {
                     handleBlueTracker()
                 }
             } )
-         }
+        }
         
 };
 
-// console.log()
-// console.log(allRedDiceArr)
-
-
 // console.log(arrayFromAllRolledDice)
-
-
-
-
-
-
-
-
 
 let general = document.querySelectorAll(".general")
 let arr = [];
