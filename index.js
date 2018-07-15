2a8e577c46d876715ae66481dfd8d9c75ec25da0
EzeBernardine <demayous2@gmail.com>
2018-07-15 16:40:33 +0100

    perfect dice initialization version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var button = document.getElementById('button');
 let redClassName = 4
 let yellowClassName = 4
 let greenClassName = 4
-let initailNumberBlue = 4
+let blueClassName = 4
    function handleRedTracker(){
     redClassName--
     document.getElementsByClassName("red-tracker")[0].innerHTML = redClassName ;
@@ -14,8 +14,8 @@ let initailNumberBlue = 4
     document.getElementsByClassName("yellow-tracker")[0].innerHTML = yellowClassName ;
    }
    function handleBlueTracker(){
-    initailNumberBlue--
-    document.getElementsByClassName("blue-tracker")[0].innerHTML = initailNumberBlue ;
+    blueClassName--
+    document.getElementsByClassName("blue-tracker")[0].innerHTML = blueClassName ;
    }
    function handleGreenTracker(){
     greenClassName--
@@ -28,29 +28,37 @@ let sum = [];
 button.onclick = function () {
     let randomNumber = Math.floor(Math.random() * 6) + 1;
     placeholder.innerHTML = randomNumber;
-    sum.push(randomNumber)
+    let pushedRandomNumber = sum.push(randomNumber)
     let lastNumber = (sum.length) - 1;
-
+    console.log(`before redand yellow is ` + sum)
          if (randomNumber === 6 ) {
             let red = document.getElementsByClassName(`red${redClassName}`)[0]
             red.addEventListener("click",function(e){
-                  e.target.parentElement.removeChild(red)
-                  handleRedTracker()
+                if(sum[sum.length-1] === 6){
+                    e.target.parentElement.removeChild(red)
+                    handleRedTracker()
+                }
             } )
             let  yellow = document.getElementsByClassName(`yellow${yellowClassName}`)[0]
             yellow.addEventListener("click",function(e){
-                e.target.parentElement.removeChild(yellow)
-                handleYellowTracker()
+                if(sum[sum.length-1] === 6){
+                     e.target.parentElement.removeChild(yellow)
+                     handleYellowTracker()
+                }
             } )
             let green = document.getElementsByClassName(`green${greenClassName}`)[0]
             green.addEventListener("click",function(e){
-                e.target.parentElement.removeChild(green)
-                handleGreenTracker()
+                if(sum[sum.length-1] === 6){
+                    e.target.parentElement.removeChild(green)
+                    handleGreenTracker()
+                }
             } )
-            let blue = document.getElementsByClassName(`blue${initailNumberBlue}`)[0]
+            let blue = document.getElementsByClassName(`blue${blueClassName}`)[0]
             blue.addEventListener("click",function(e){
-                e.target.parentElement.removeChild(blue)
-                handleBlueTracker()
+                if(sum[sum.length-1] === 6 ){
+                    e.target.parentElement.removeChild(blue)
+                    handleBlueTracker()
+                }
             } )
          }
 };
@@ -71,7 +79,8 @@ button.onclick = function () {
 
 let general = document.querySelectorAll(".general")
 let arr = [];
-for (let i = 0; i < general.length; i++) {
+let i
+for (i = 0; i < general.length; i++) {
     general[i].addEventListener('click', function () {
         general[i].className = "green";
         arr.push(general[i]);
